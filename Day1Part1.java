8054e4c27ee835e47bdbf70c3f45f38f5439c3da
Steven Goldburg <59116258+goldyVA@users.noreply.github.com>
2025-12-04 06:31:59 -0600

    Update and rename Day1Part1_2025.java to Day1Part1.java

--- Day1Part1.java
+++ Day1Part1.java
@@ -3,7 +3,7 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.*; // Import the Scanner class to read text files
 
-public class Day1Part1_2025
+public class Day1Part1
 {
   public static void main(String[] args) 
   {
